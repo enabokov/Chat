0154bc072c7632dd627b07977a22b2615c654a17
enabokov <edwardnabokov@gmail.com>
2018-01-31 00:08:25 +0200

    Add TemplateMiddleware

--- service/middlewares.py
+++ service/middlewares.py
@@ -1,10 +1,37 @@
-# from aiohttp import web
-# from aiohttp_jinja2 import render_template
+from aiohttp import web
+
+from misc.handlers import TemplateHandler
 
 
-# @web.middleware
-# async def middleware(request, handler):
-#     try:
-#         response = await handler(request)
-#     except web.HTTPUnauthorized as exc:
-#         return
+class TemplateMiddleware:
+
+    encoding = 'utf-8'
+    handler = TemplateHandler()
+
+    @web.middleware
+    async def middleware(self, request, handler):
+        try:
+            return await handler(request)
+        except web.HTTPUnauthorized as exc:
+            return self.handler.render_template(
+                template_name='errors/Unauthorized.html',
+                request=request,
+                context={'error': exc.reason},
+                status=exc.status,
+            )
+
+        except web.HTTPNotFound as exc:
+            return self.handler.render_template(
+                template_name='errors/NotFound.html',
+                request=request,
+                context={'error': exc.reason},
+                status=exc.status,
+            )
+
+        except web.HTTPBadRequest as exc:
+            return self.handler.render_template(
+                template_name='errors/BadRequest.html',
+                request=request,
+                context={'error': exc.reason},
+                status=exc.status,
+            )
